a87f5e40191d2d6620324cab80c63c243ac5ef72
Gregoire BURNET <44721323+Webgburnet@users.noreply.github.com>
2019-03-18 10:46:32 +0100

    update 2019.03.18

--- Arduino/WAP_Network/WAP_Network.ino
+++ Arduino/WAP_Network/WAP_Network.ino
@@ -1,7 +1,9 @@
 #include <WiFi.h>
 
-char ssid[] = "Sti-2k18-SIN";      // Le nom du réseau
+char ssid[] = "Sti-2k18-SIN";      // Le nom du réseau wifi
+char pass[] = "Sti-2k18-Sin";        // Mot de passe du réseau wifi
 int status = WL_IDLE_STATUS;     // le statut de la radio Wifi
+IPAddress ip_shield;
 
 void setup() {
   // initialise le moniteur série:
@@ -9,8 +11,9 @@ void setup() {
 
   // tentative de connexion à un réseau ouvert:
   Serial.println("Tentative de connexion pour ouvrir un réseau ...");
-  status = WiFi.begin(ssid);
-
+  status = WiFi.begin(ssid, pass);
+  ip_shield = WiFi.localIP();
+  //WiFi.config(ip_shield);
   // si vous n'êtes pas connecté, arrêtez-vous ici:
   if ( status != WL_CONNECTED) {
     Serial.println("Impossible d'obtenir une connexion wifi ! ");
